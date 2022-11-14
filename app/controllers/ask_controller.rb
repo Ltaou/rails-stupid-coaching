8edbe1f93f6e4a51a4050b3d7d100b23a232c597
root <leonidastaousanis76@gmail.com>
2022-11-14 12:32:36 +0100

    fuck

---
+++ app/controllers/ask_controller.rb
@@ -0,0 +1,17 @@
+class AskController < ApplicationController
+  def action
+
+  end
+  def answer
+    @ask = params["ask"]
+    @answer = ''
+
+    if @ask == 'I am going to work'
+        @answer = 'Great!'
+    elsif @ask.include? ("?")
+        @answer = 'Silly question, get dressed and go to work!'
+    else
+        @answer = " I don't care, get dressed and go to work!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "ask#home"
+  get "/ask", to: "ask#action"
+  get "/answer", to: "ask#answer"
 end
